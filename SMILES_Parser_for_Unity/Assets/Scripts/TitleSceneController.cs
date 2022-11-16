eaf8f08802ca70f3b046576ddd1ba1fa7b8cb88b
Sound piercer Official <soundpiercer@gmail.com>
2022-11-17 01:57:09 +0900

    [UI] Implement Menu Frame Toggle Buttons

--- SMILES_Parser_for_Unity/Assets/Scripts/TitleSceneController.cs
+++ SMILES_Parser_for_Unity/Assets/Scripts/TitleSceneController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleSceneController : MonoBehaviour
 {
@@ -8,10 +9,41 @@ public class TitleSceneController : MonoBehaviour
     public RectTransform panel;
     public GameObject loadingPanel;
 
+    [Header("Menu Frame Area")]
+    public List<GameObject> menuFrameButtons;
+
+
     public static List<string> smiles;
 
-    private bool useAIServer = false;
+    #region CONSTANT
     private const string AI_SERVER_HOST = "";
+
+    private readonly Color32 BUTTON_SELECTED = new Color32(0xFF, 0xF3, 0xC2, 0xFF);
+    private readonly Color32 BUTTON_NORMAL = Color.white;
+    #endregion
+
+    private void Start()
+    {
+        SetMenuFrameButton(0);
+    }
+
+    // Toggle Menu Frame Area Buttons.
+    public void SetMenuFrameButton(int mode)
+    {
+        for (int i = 0; i < menuFrameButtons.Count; i++)
+        {
+            if (i == mode)
+            {
+                menuFrameButtons[i].GetComponent<Button>().interactable = false;
+                menuFrameButtons[i].GetComponent<Image>().color = BUTTON_SELECTED;
+            }
+            else
+            {
+                menuFrameButtons[i].GetComponent<Button>().interactable = true;
+                menuFrameButtons[i].GetComponent<Image>().color = BUTTON_NORMAL;
+            }
+        }
+    }
 
     public void Generate()
     {
